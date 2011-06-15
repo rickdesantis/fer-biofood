7d980caa376a55306efa54ca7ad21ff100e3abbb
effetti@gmail.com <effetti@gmail.com@01eb7cd7-d586-c20d-1d2d-edba3df71e34>
2011-06-15 14:12:49 +0000

    Definiti tutti i servizi per aggiungere entità, da utilizzare direttamente nei vari controller.

--- Fer-BioFood/src/it/aip/mcdonald/service/ManageWorld.java
+++ Fer-BioFood/src/it/aip/mcdonald/service/ManageWorld.java
@@ -8,6 +8,7 @@ import com.google.appengine.api.datastore.Transaction;
 import it.aip.mcdonald.meta.*;
 import it.aip.mcdonald.model.*;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -79,6 +80,13 @@ public class ManageWorld {
             Datastore.delete(u.getKey());
             tx.commit();
         }
+        BuonoPerEsigenzaMeta f = BuonoPerEsigenzaMeta.get();
+        List<BuonoPerEsigenza> list2 = Datastore.query(f).asList();
+        for (BuonoPerEsigenza u : list2) {
+            Transaction tx = Datastore.beginTransaction();
+            Datastore.delete(u.getKey());
+            tx.commit();
+        }
         
         // Aggiungo le nuove entità appena create, prendendo i nomi hardcoded
         String[] nomi = {"celiaci", "vegani", "vegetariani", "esigenza1"};
@@ -103,11 +111,21 @@ public class ManageWorld {
         }
         
         // Aggiungo le nuove entità
-        Produttore p1 = aggiungiProduttore("Desantis", "allevatore", "È un allevatore di mucche, galline, etc.", "via delle Leghe 16, Milano (MI)", "effetti@gmail.com", "1234");
-        Produttore p2 = aggiungiProduttore("Gerola", "agricoltore tipo1", "È l'agricoltore più in del momento, con le sue coltivazioni di marijuana, etc.", "piazza Duomo, Milano (MI)", "filippo.gerola@gmail.com", "1234");
-        Produttore p3 = aggiungiProduttore("Pedrotti", "allevatore", "Alleva da tanti anni le stesse galline, ma, si sa, gallina vecchia fa buon brodo!", "piazza Leonardo da Vinci 32, Milano (MI)", "edobounce@gmail.com", "1234");
-        
-        Datastore.put(p1, p2, p3);
+        aggiungiProduttore("Desantis",                                  // nome
+                           "allevatore",                                // tipo
+                           "È un allevatore di mucche, galline, etc.",  // descrizione
+                           "via delle Leghe 16, Milano (MI)",           // indirizzo
+                           "effetti@gmail.com", "1234");                // email e password
+        aggiungiProduttore("Gerola",
+                           "agricoltore tipo1",
+                           "È l'agricoltore più in del momento, con le sue coltivazioni di marijuana, etc.",
+                           "piazza Duomo, Milano (MI)",
+                           "filippo.gerola@gmail.com", "1234");
+        aggiungiProduttore("Pedrotti",
+                           "allevatore",
+                           "Alleva da tanti anni le stesse galline, ma, si sa, gallina vecchia fa buon brodo!",
+                           "piazza Leonardo da Vinci 32, Milano (MI)",
+                           "edobounce@gmail.com", "1234");
     }
     
     private void initProdotti() {
@@ -119,14 +137,30 @@ public class ManageWorld {
             Datastore.delete(u.getKey());
             tx.commit();
         }
+        OffertaMeta f = OffertaMeta.get();
+        List<Offerta> list2 = Datastore.query(f).asList();
+        for (Offerta u : list2) {
+            Transaction tx = Datastore.beginTransaction();
+            Datastore.delete(u.getKey());
+            tx.commit();
+        }
         
         // Aggiungo le nuove entità
+        aggiungiProdotto("Fiorentina di manzo",         // nome
+                         "Desantis",                    // produttore
+                         "carne",                       // tipo
+                         "La classica carne toscana!",  // descrizione
+                         "Fa ingrassare ai livelli!",   // info nutrizionali
+                         new String[] {});              // array di esigenze
+        aggiungiProdotto("Uova di gallina",
+                         "Desantis",
+                         "carne",
+                         "Le uova biologiche più fresche e buone!",
+                         "Valori medi per 100 grammi senza guscio:\nValore energetico: Kcal 128 (KJ 535)\nProteine: g 12,4\nCarboidrati: g 0,5\nGrassi: g 8,7",
+                         new String[] {"vegetariani"});
         
-        // nome, produttore, tipo, descr, infoNutrizionali
-        
-        Prodotto p1 = aggiungiProdotto("Fiorentina", "Desantis", "carne", "La classica carne toscana!", "Fa ingrassare ai livelli!");
-        
-        Datastore.put(p1);
+        aggiungiOfferta("Uova di gallina",  // prodotto
+                        "15");              // giorni di offerta
     }
     
     public Produttore aggiungiProduttore(String nome, String tipo, String descr, String indirizzo, String mail, String password) {
@@ -154,13 +188,14 @@ public class ManageWorld {
         return prod;
     }
     
-    public Prodotto aggiungiProdotto(String nome, String produttore, String tipo, String descr, String infoNutrizionali) {
+    public Prodotto aggiungiProdotto(String nome, String produttore, String tipo, String descr, String infoNutrizionali, String[] esigenze) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("nome", nome);
         map.put("tipo", tipo);
         map.put("descr", descr);
         map.put("infoNutrizionali", infoNutrizionali);
         map.put("produttore", produttore);
+        map.put("esigenze", esigenze);
         return aggiungiProdotto(map);
     }
     
@@ -177,9 +212,42 @@ public class ManageWorld {
             List<Produttore> list = Datastore.query(e).filter(e.nome.equal((String)param.get("produttore"))).asList();
             prod.getProduttoreRef().setModel(list.get(0));
         }
+        for (String esig : (String[]) param.get("esigenze")) {
+            EsigenzaMeta e = EsigenzaMeta.get();
+            List<Esigenza> list = Datastore.query(e).filter(e.nome.equal(esig)).asList();
+            BuonoPerEsigenza bpe = new BuonoPerEsigenza();
+            bpe.getEsigenzaRef().setModel(list.get(0));
+            bpe.getProdottoRef().setModel(prod);
+            Transaction tx = Datastore.beginTransaction();
+            Datastore.put(bpe);
+            tx.commit();
+        }
         Transaction tx = Datastore.beginTransaction();
         Datastore.put(prod);
         tx.commit();
         return prod;
+    }
+    
+    public Offerta aggiungiOfferta(String prodotto, String giorniOfferta) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("prodotto", prodotto);
+        map.put("giorniOfferta", giorniOfferta);
+        return aggiungiOfferta(map);
+    }
+    
+    public Offerta aggiungiOfferta(Map<String, Object> param) {
+        Offerta of = new Offerta();
+        {
+            ProdottoMeta e = ProdottoMeta.get();
+            List<Prodotto> list = Datastore.query(e).filter(e.nome.equal((String)param.get("prodotto"))).asList();
+            of.getProdottoRef().setModel(list.get(0));
+        }
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) + Integer.parseInt((String)param.get("giorniOfferta")));
+        of.setFineOfferta(c.getTime());
+        Transaction tx = Datastore.beginTransaction();
+        Datastore.put(of);
+        tx.commit();
+        return of;
     }
 }
